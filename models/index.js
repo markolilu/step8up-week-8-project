96b01b27529dd9b21d1d02275659d4b02a90b1f1
markolilu <marklumbpt@gmail.com>
2026-03-03 08:43:36 +0000

    added all relevant files to relevant folders. || will now begin work on building the models to be used later

---
+++ models/index.js
@@ -0,0 +1,10 @@
+// // import all models
+// const Post = require("./post");
+// const Category = require("./category");
+// const User = require("./user");
+
+// use above as reference for how to set up foreign key relationships betweem
+
+// Post, Category and User models
+// consider adding more later.
+
