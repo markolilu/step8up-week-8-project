6d63e132155f81ec7109530d7a18a4c655c25555
markolilu <marklumbpt@gmail.com>
2026-03-04 15:51:57 +0000

    built index.js // will need to test this later

--- models/index.js
+++ models/index.js
@@ -8,3 +8,27 @@
 // Post, Category and User models
 // consider adding more later.
 
+const Post = require('./post');
+const Category = require('./category');
+const User = require('./user');
+
+Post.BelongsTo(User, {
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE'
+});
+
+User.HasMany(Post, {
+    foreignKey: 'user_id'
+});
+
+Post.BelongsTo(Category, {
+    foreignKey: 'category_id',
+    as: 'category',
+});
+
+Category.HasMany(Post, {
+    foreignKey: 'category_id'
+    as: 'posts'
+});
+
+module.exports = {Post, Category, User};
