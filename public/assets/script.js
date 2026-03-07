e1910ef81aed9dcc45b62885ff7bff4b5cc176e2
markolilu <marklumbpt@gmail.com>
2026-03-07 19:53:25 +0000

    Added create and fetch posts in Script.js. || need to add functionality to Update and Delete posts by :id

--- public/assets/script.js
+++ public/assets/script.js
@@ -55,9 +55,53 @@ function logout() {
 }
 
 
-function createPost()
+function createPost() {
+    const title = document.getElementById('postTitle').value;
+    const content = document.getElementById('postContent').value;
+    const category = document.getElementById('postCategory').value;
+    fetch("http://localhost:3000/api/posts", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Authorization": `Bearer ${token}`
+        },
+        body: JSON.stringify({ title, content, category, postedBy: "user" })
+    })
+    .then((response) => response.json())
+    .then(data => {
+        console.log("Post created:", data);
+    })
+    .catch(error => {
+        console.error("Error creating post:", error);
+    });
+}
 
-function fetchPosts()
+function fetchPosts() {
+    fetch("http://localhost:3000/api/posts", {
+        method: "GET",
+        headers: {
+            "Content-Type": "application/json",
+            "Authorization": `Bearer ${token}`
+        }
+    })
+    .then((response) => response.json())
+    .then(data => {
+        console.log("Posts fetched:", data);
+        const postsContainer = document.getElementById('posts-list');
+        postsContainer.innerHTML = '';
+        data.forEach(post => {
+            const postElement = document.createElement('div');
+            dispatchEvent.innerHTML = `<h3>${post.title}</h3>
+                                      <p>${post.content}</p>
+                                      <p>Category: ${post.category}</p>
+                                      <p>Posted by: ${post.postedBy}</p>`;
+            postsContainer.appendChild(postElement);
+        });
+    })
+    .catch(error => {
+        console.error("Error fetching posts:", error);
+    });
+}
 
 function updatePost(postId)
 
