680d65a6564f40a30d2bf80046f7228863d72e03
markolilu <marklumbpt@gmail.com>
2026-03-10 12:47:07 +0000

    I am a muppet and now know why nothing was working. I didn't put anything in the server.js file THIS WHOLE TIME!!! ... but its ok we move and learn. :)) :((

--- models/index.js
+++ models/index.js
@@ -1,13 +1,3 @@
-// // import all models
-// const Post = require("./post");
-// const Category = require("./category");
-// const User = require("./user");
-
-// use above as reference for how to set up foreign key relationships betweem
-
-// Post, Category and User models
-// consider adding more later.
-
 const Post = require('./post');
 const Category = require('./category');
 const User = require('./user');
---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const path = require("path");
+
+const sequelize = require("./config/connections.js");
+const routes = require("./routes.js");
+
+const app = express();
+app.use(bodyParser.json());
+app.use(express.urlencoded({extended: true}));
+
+const PORT = process.env.PORT || 3306;
+
+const rebuild = process.argv[2] === "--rebuild";
+
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "public", "index.htm"));
+});
+
+app.use(routes);
+
+sequelize.sync({force: rebuild}).then(() => {
+    app.listen(PORT, () => console.log("Now listening"));
+});
